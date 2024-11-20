14cfec8e97686a49d4abb57324a3b72e78465b13
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-20 10:48:08 +0900

    build: ツール追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
+import Inspect from 'vite-plugin-inspect';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,6 +11,7 @@ export default defineConfig({
       open: true, // ビルド後に自動でブラウザで開く
       filename: "bundle-analysis.html", // 出力ファイル名
     }),
+    Inspect(),
   ],
   base: "./",
   build: {
