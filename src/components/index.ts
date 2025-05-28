01a87c178eff90ab5db59cd587cc1d887e0ccd05
yuto ichinose <yuto.0526.sano@gmail.com>
2025-05-28 23:28:49 +0900

    docs: index.tsにsideEffectsに関するコメントを追加

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,6 @@ export { ComponentC } from "./component-c";
 console.log("side effect"); // "sideEffects": falseにしないと実行されるし、このファイルごと含まれる
 
 export const hello = () => console.log("hello") // importされると"sideEffects": falseしてもこのファイルごと含まれる
+
+// ここでA,B,Cのコードを定義してexportしてると、同じファイルに定義してるので、
+// "sideEffects": falseにしても、A,B,Cのコードは含まれる
